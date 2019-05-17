e8d9d4063bdee06c97abc61b9c09c962d13d73aa
ramiro.vargassalas <ramiro.vargassalas@gmail.com>
2019-05-17 16:37:32 -0500

    test marcar listo version (RED)

--- sisred_app/tests.py
+++ sisred_app/tests.py
@@ -895,6 +895,24 @@ class VersionMarcarTestCase(TestCase):
         self.assertEqual(versionMainAfter1.es_final, False)
         self.assertEqual(versionMainAfter2.es_final, True)
 
+    def testMarcarComoVersionLista(self):
+        url1 = '/api/versiones/'
+        url2 = '/marcar'
+        versionId = "1"
+        url = url1 + versionId + url2
+
+        fecha = datetime.datetime.now()
+        proyecto = ProyectoConectate.objects.create(id=1, fecha_inicio=fecha, fecha_fin=fecha)
+        red = RED.objects.create(id=1, proyecto_conectate=proyecto)
+        versionMain = Version.objects.create(id=1, es_final=False, numero=1, red=red)
+
+        response = self.client.post(url, format='json')
+
+        versionMainAfter = Version.objects.get(id=1)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(versionMainAfter.es_final, True)
+
 
 class sisRedTestCase(TestCase):
 
